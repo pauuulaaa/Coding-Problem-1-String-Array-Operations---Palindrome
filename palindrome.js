3d7292bdcf7664bdad6fe6afb7dda572c6d271c6
pauuulaaa <heleinapaula@gmail.com>
2024-12-23 14:17:06 +0800

    palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,9 @@
+function openProg() {
+    let racecar = prompt("ENTER RACECAR");
+    let recorder = prompt("ENTER RECORDER");
+    alert("Original string: " + racecar)
+    alert("Original string: " + recorder)
+    const reverse = (racecar) => racecar.split("").reverse().join("");
+    const reverseString = (recorder) => recorder.split("").reverse().join("");
+    alert("Reversed String: " + (reverse(racecar)));
+    alert("Reversed String: " + (reverseString(recorder)));
